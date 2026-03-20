e17c6ca63d7c928dc3590d153490b6aa15e78551
дюша <>
2026-03-20 16:25:20 +0300

    qwweqweqwe

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from api.food.views import redirect_short_link
+from api.food.views import get_link
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<str:code>/', redirect_short_link, name='short-link-redirect'),
+    path('s/<str:code>/', get_link, name='short-link-redirect'),
 ]
 
 if settings.DEBUG:
