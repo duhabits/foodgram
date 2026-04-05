20dea6187fbe8737022b4e32ce3a98c5a4ec4a61
duha <jerebtsow.a@yandex.ru>
2026-04-05 16:57:43 +0300

    asdasd

--- backend/foodgram/api/food/utils.py
+++ backend/foodgram/api/food/utils.py
@@ -1,11 +1,11 @@
 def generate_shopping_cart_content(ingredients):
-    lines = ['Список покупок\n', '=' * 50 + '\n\n']
+    lines = ('Список покупок\n', '=' * 50 + '\n\n')
 
     for ingredient in ingredients:
         lines.append(
-            f'{ingredient['ingredient__name']} - '
-            f'{ingredient['total_amount']} '
-            f'{ingredient['ingredient__measurement_unit']}\n'
+            f'{ingredient('ingredient__name')} - '
+            f'{ingredient('total_amount')} '
+            f'{ingredient('ingredient__measurement_unit')}\n'
         )
 
     lines.append('\n' + '=' * 50)
