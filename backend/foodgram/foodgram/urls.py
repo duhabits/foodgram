5f7385b83fc24497b443ce3d2f49c79fd9b4cf31
duha <jerebtsow.a@yandex.ru>
2026-04-12 16:53:17 +0300

    asd

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,10 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
+from django.urls import include, path
 
 from api.food.views import short_link_redirect
 
